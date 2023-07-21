c56b9c6fe68c62ca8fd7b298eb8a9e96d3fb94ff
ZH <zharov.2010@yandex.ru>
2023-07-21 14:16:44 +0300

    dz9

--- dz8/z2.py
+++
@@ -1,8 +0,0 @@
-# Прочитайте созданный в прошлом задании csv файл без использования csv.DictReader. Распечатайте его как pickle строку.
-import pickle
-
-
-
-
-new_dict = pickle.loads(d)
-print(f'{new_dict = }')
